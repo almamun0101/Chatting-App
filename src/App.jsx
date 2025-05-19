05dc2da99c0ae300a0fb018209e5e6fef519aca2
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-05-20 01:47:22 +0600

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
-      
+      <Home/>
+      {/* <img class="j2x7_17hqRVmwte_tWFa RV8RoaI_SlEMC5CEQ3ms s39ClfdnICIuO9QZ5YG_" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/authentication/illustration.svg" alt="illustration"></img> */}
     </div>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SingIn from './component/SingIn'
+import SignUp from './component/SignUp'
+
+const Home = () => {
+  return (
+    <div>
+        {/* <SingIn/> */}
+        <SignUp/>
+    </div>
+  )
+}
+
+export default Home
